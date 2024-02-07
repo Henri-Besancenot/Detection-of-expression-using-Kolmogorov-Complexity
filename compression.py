7e587958d0dc63056542ce3e853dc252d431e6c5
Henri <henri.besancenotel@gmail.com>
2024-02-07 11:26:58 +0100

    initialisation du repo git

---
+++ compression.py
@@ -0,0 +1,6 @@
+# The purpose of this file is to compress images.
+# We should be able to :
+#   - compress together different images.
+#   - get the images sizes.
+#   - get few indicators such as NCD
+# To train the model we're going to use FER-2013 Dataset
